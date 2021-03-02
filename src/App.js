4c3d973df6fa93fa34bf04d017a027057c515a49
Bruno-Viana <bvianascs@gmail.com>
2021-03-02 03:58:06 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect,useRef} from 'react';
+import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
 //Páginas:
